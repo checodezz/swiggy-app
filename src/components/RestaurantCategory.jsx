48421eb6c012c817b42727230819bf240133c762
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-07-06 22:53:07 +0530

    state lifting

--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
 import ItemList from "./ItemList";
 
-const RestaurantCategory = ({ data, showItems }) => {
-  //   console.log(data);
-
-  //   const [showItems, setShowItems] = useState("");
-
+const RestaurantCategory = ({ data, showItems, setShowIndex }) => {
+  // const [showItems, setShowItems] = useState("");
+  const handleClick = () => {
+    setShowIndex(!showItems);
+  };
   return (
     <div>
       <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4">
         <div
           className="flex justify-between cursor-pointer"
-          //   onClick={handleClick}
+          onClick={handleClick}
         >
           <span className="font-medium text-lg">
             {data.title} ({data.itemCards.length})
